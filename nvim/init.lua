20572b09b6278dbe8aff60b3b2a3cff28cb62685
F4isalSH <f4isalcode@gmail.com>
2026-06-10 10:30:43 +0300

    fix: reload nvim on buffer switch

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,9 +4,16 @@ vim.o.number = true
 vim.o.relativenumber = true
 
 vim.o.autoread = true
-vim.api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
 	pattern = "*",
-	command = "checktime",
+	command = "if mode() != 'c' | checktime | endif",
+})
+
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+	pattern = "*",
+	callback = function()
+		vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.WARN)
+	end,
 })
 
 require("plugins")
